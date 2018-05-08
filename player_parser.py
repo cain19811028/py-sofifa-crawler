6605f3e77d2101201d8b403dd1b255a6df240aa9
Cain HC Chen <cainhc.chen@moxa.com>
2018-05-08 17:50:38 +0800

    parse full name and short name

--- player_parser.py
+++ player_parser.py
@@ -10,7 +10,15 @@ def parse_player_data(player_id):
 
     response = requests.get(url, headers = HEADERS)
     content = html.fromstring(response.text)
-    print(content)
+
+    # name
+    temp = content.xpath('//div[@class="player"]')[0]
+    temp = temp.xpath('//div[@class="info"]')[0]
+    short_name = temp.xpath('//h1')[0].text_content().split(' (')[0]
+    full_name = temp.xpath('//div[@class="meta"]/span')[0].text_content()
+    full_name = full_name.split('Age ')[0].split('  CF ST RW')[0]
+    print(short_name)
+    print(full_name)
 
 """
 Main
