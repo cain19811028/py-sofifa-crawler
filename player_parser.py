febd1565c254c397ac32fcd879c390ea5845eef0
cain19811028 <cain19811028@gmail.com>
2018-05-08 20:19:12 +0800

    parse birthday, height, weight data

--- player_parser.py
+++ player_parser.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from lxml import html
 
 DOMAIN = "https://sofifa.com/"
@@ -11,14 +12,27 @@ def parse_player_data(player_id):
     response = requests.get(url, headers = HEADERS)
     content = html.fromstring(response.text)
 
-    # name
+    # short_name, full_name
     temp = content.xpath('//div[@class="player"]')[0]
-    temp = temp.xpath('//div[@class="info"]')[0]
-    short_name = temp.xpath('//h1')[0].text_content().split(' (')[0]
-    full_name = temp.xpath('//div[@class="meta"]/span')[0].text_content()
-    full_name = full_name.split('Age ')[0].split('  CF ST RW')[0]
+    info = temp.xpath('//div[@class="info"]')[0]
+    short_name = info.xpath('//h1')[0].text_content().split(' (')[0]
+    meta = info.xpath('//div[@class="meta"]/span')[0].text_content()
+    full_name = meta.split('Age ')[0].split('  ')[0]
     print(short_name)
     print(full_name)
+
+    # birthday, height, weight
+    data = meta.split('Age ')[1]
+    data = data.split(') ')
+    birthday = data[0].split('(')[1]
+    birthday = time.mktime(time.strptime(birthday, '%b %d, %Y'))
+    birthday = time.strftime("%Y%m%d", time.gmtime(birthday))
+    data = data[1].split(' ')
+    height = data[0].replace("cm", "")
+    weight = data[1].replace("kg", "")
+    print(birthday)
+    print(height)
+    print(weight)
 
 """
 Main
