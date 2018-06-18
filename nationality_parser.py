76d6e8e8473b430de978d49532729f98aae35ae1
cain19811028 <cain19811028@gmail.com>
2018-06-18 20:59:06 +0800

    get all time nationality

--- nationality_parser.py
+++ nationality_parser.py
@@ -11,24 +11,32 @@ DOMAIN = "https://sofifa.com"
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 def get_all_time_node():
+
     response = requests.get(DOMAIN, headers = HEADERS)
     content = html.fromstring(response.text)
-    time_node = content.xpath('//div[@class="card-body"]/a/@href')
-    return time_node
+
+    return content.xpath('//div[@class="card-body"]/a/@href')
 
 def get_all_nationality(time_node):
-    test = time_node[0]
-    url = DOMAIN + test
-    response = requests.get(url, headers = HEADERS)
-    content = html.fromstring(response.text)
-    select = content.xpath('//select[@name="na[]"]')[0]
-    options = select.xpath('.//option')
-
+    
+    index = 1
     result = {}
-    for opt in options:
-        key = opt.xpath('.//@value')
-        if len(key) > 0:
-            result[key[0]] = opt.text_content()
+    print("time node count : " + str(len(time_node)))
+    for link in time_node:
+        url = DOMAIN + link
+        print(str(index) + ". " + url)
+        response = requests.get(url, headers = HEADERS)
+        content = html.fromstring(response.text)
+        select = content.xpath('//select[@name="na[]"]')[0]
+        options = select.xpath('.//option')
+
+        for opt in options:
+            key = opt.xpath('.//@value')
+            if len(key) > 0:
+                result[key[0]] = opt.text_content()
+
+        index += 1
+        time.sleep(1)
 
     return result
 
