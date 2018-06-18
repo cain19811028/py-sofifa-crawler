d043df3408df91debf8692101045cd5287086beb
cain19811028 <cain19811028@gmail.com>
2018-06-18 18:06:34 +0800

    test first time node

--- nationality_parser.py
+++ nationality_parser.py
@@ -6,12 +6,22 @@ import time
 from dao import Dao
 from lxml import html
 
-DOMAIN = "https://sofifa.com/"
+DOMAIN = "https://sofifa.com"
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
-def get_all_nationality():
+def get_all_time_node():
     response = requests.get(DOMAIN, headers = HEADERS)
     content = html.fromstring(response.text)
+    time_node = content.xpath('//div[@class="card-body"]/a/@href')
+    return time_node
+
+def get_all_nationality(time_node):
+    test = time_node[0]
+    print(test)
+    url = DOMAIN + test
+    response = requests.get(url, headers = HEADERS)
+    content = html.fromstring(response.text)
+    print(content.text_content())
     nationality_list = []
     return nationality_list
 
@@ -23,5 +33,6 @@ if __name__ == "__main__":
     Dao.init()
     Dao.create_sofifa_nationality()
 
-    nationality_list = get_all_nationality()
+    time_node = get_all_time_node()
+    nationality_list = get_all_nationality(time_node)
     print(nationality_list)
