b35b2a8e9ab7d22c93f1d3f2f8626dc288991e23
cain19811028 <cain19811028@gmail.com>
2018-07-08 12:00:50 +0800

    medify change_log rating parser

--- player_parser.py
+++ player_parser.py
@@ -66,20 +66,19 @@ def parse_rating_data(player_id):
 
     # change_log
     index = 0
-    change_log = content.xpath('//article[@class="column"]/dl')
+    change_log = content.xpath('//article/dl[@class="change-log"]')
     if len(change_log) > 0:
         change_log = change_log[0]
-        dt = change_log.xpath('//dt')
-        dd = change_log.xpath('//dd')
-        for d in dd:
-            if "Overall Rating" in d.text_content():
-                date = dt[index].text_content()[-14:].strip()
+        h5 = change_log.xpath('.//h5')
+        columns = change_log.xpath('.//div[@class="columns"]')
+        for col in columns:
+            if "Overall Rating" in col.text_content():
+                date = h5[index].text_content()[-12:].strip()
                 date = datetime.datetime.strptime(date, '%b %d, %Y')
                 date = date.strftime('%Y%m%d')
                 
-                rating = d.text_content().split('Overall Rating ')[1]
-                rating = rating.split('  ')[1].split(' ')[0]
-                rating_record[date] = rating.replace('\r\n', '')
+                rating = col.text_content().split('Overall Rating ')[1][:2]
+                rating_record[date] = rating
             index += 1
 
     rating_record = json.dumps(convert_rating_data(rating_record))
@@ -182,9 +181,8 @@ if __name__ == "__main__":
      45 = Juventus
     """
     # player_set = get_all_time_player_by_team_id(10)
-    # player_set = get_player_by_team_id(10)
-    # print("player count : " + str(len(player_set)))
-    player_set = {146439, 189963, 186380, 186382, 4111, 186384, 186385, 186386, 225811, 220182, 220185, 210970, 210969, 181786, 13850, 50723, 220198, 218667, 17964, 211501, 198190, 560, 198193, 205362, 181820, 186942, 190531, 20551, 170570, 9805, 229968, 202832, 204884, 233047, 233048, 203864, 163415, 163419, 2651, 168542, 140384, 213089, 183907, 2148, 192613, 137829, 614, 211048, 146536, 164462, 229495, 218744, 231032, 218745, 218746, 237692, 51321, 183427, 153079, 225414, 199304, 211593, 211594, 211595, 51336, 211597, 4233, 201359, 232080, 221329, 175254, 101015, 143001, 221340, 188572, 221342, 221343, 158, 221350, 223912, 183465, 6826, 164009, 138412, 224947, 230068, 224949, 230070, 224951, 24248, 230081, 180930, 230084, 150724, 25798, 141001, 233164, 197837, 52941, 146641, 208594, 212692, 223963, 189668, 5860, 158438, 223977, 223978, 223979, 208622, 189678, 189679, 239, 189682, 189684, 50421, 189686, 189687, 201975, 169721, 212218, 200441, 189691, 189692, 169214, 189693, 216320, 216321, 216322, 186627, 216324, 216325, 2307, 163587, 230666, 185103, 186127, 195858, 215316, 172820, 222492, 221982, 135455, 3363, 50467, 229670, 203574, 139062, 206134, 193848, 53050, 149306, 197948, 171833, 206654, 20289, 134979, 3395, 184134, 53578, 196941, 137551, 210257, 135507, 209750, 229718, 5471, 204639, 229217, 190821, 190822, 183142, 190824, 205161, 205162, 205163, 165740, 183145, 192366, 190823, 119152, 26992, 165239, 235904, 210423, 169718, 205192, 199561, 205193, 1419, 194958, 155539, 222104, 202652, 182696, 169725, 111023, 138671, 199602, 225719, 142784, 450, 139720, 136137, 46027, 232396, 228813, 174543, 136144, 138193, 236499, 192985, 188377, 161754, 39386, 223197, 209886, 199135, 202721, 50660, 227813, 138726, 12265, 152554, 220651, 186345, 220654, 496, 231410, 199159, 210424, 210425, 1533}
+    player_set = get_player_by_team_id(10)
+    print("player count : " + str(len(player_set)))
     print(player_set)
 
     for player_id in player_set:
